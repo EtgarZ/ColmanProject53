c8bb26b49c0b94e009ac583e28a6afe70bed2de7
EtgarZ <etgarzyl92@gmail.com>
2019-03-06 23:07:13 +0200

    handle return key press without providing image to process

--- Client/CardReaderApp/app/src/main/java/com/cardreaderapp/activities/MainActivity.java
+++ Client/CardReaderApp/app/src/main/java/com/cardreaderapp/activities/MainActivity.java
@@ -75,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         try {
+            if (data == null)
+                return;
+            
             Button btnExtractInfo = findViewById(R.id.btnExtractInfo);
             btnExtractInfo.setVisibility(View.VISIBLE);
 
