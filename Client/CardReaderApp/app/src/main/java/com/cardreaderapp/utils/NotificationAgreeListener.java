f3f619570d23985a21e6e53a7955133530fbbb6c
EtgarZ <etgarzyl92@gmail.com>
2019-06-03 00:58:32 +0300

    fix share issue

--- Client/CardReaderApp/app/src/main/java/com/cardreaderapp/utils/NotificationAgreeListener.java
+++ Client/CardReaderApp/app/src/main/java/com/cardreaderapp/utils/NotificationAgreeListener.java
@@ -31,33 +31,25 @@ public class NotificationAgreeListener extends BroadcastReceiver {
         mContext = context;
         mFromUserId = intent.getExtras().getString("from_user_id");
         mToUserId = intent.getExtras().getString("to_user_id");
-        addCards();
-    }
 
-    private void addCards()
-    {
         FirebaseDatabase.getInstance().getReference("Users/" + mFromUserId + "/Cards").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Vector<Card> cards = new Vector<>();
                 for (DataSnapshot cs : dataSnapshot.getChildren()) {
 
-                        Card c = new Card();
-                        c.setPersonName(cs.child("personName").getValue().toString());
-                        c.setPhoneNumber(cs.child("phoneNumber").getValue().toString());
-                        c.setCompany(cs.child("company").getValue().toString());
-                        c.setAddress(cs.child("address").getValue().toString());
-                        c.setEmail(cs.child("email").getValue().toString());
-                        c.setWebsite(cs.child("website").getValue().toString());
-                        c.setImageUri(cs.child("imageUri").getValue().toString());
-                        cards.add(c);
+                    Card c = new Card();
+                    c.setPersonName(cs.child("personName").getValue().toString());
+                    c.setPhoneNumber(cs.child("phoneNumber").getValue().toString());
+                    c.setCompany(cs.child("company").getValue().toString());
+                    c.setAddress(cs.child("address").getValue().toString());
+                    c.setEmail(cs.child("email").getValue().toString());
+                    c.setWebsite(cs.child("website").getValue().toString());
+                    c.setImageUri(cs.child("imageUri").getValue().toString());
+                    cards.add(c);
                 }
-
-                for(Card card: cards){
-                    String cardId = FirebaseDatabase.getInstance().getReference("Users/" + mToUserId + "/Cards").push().getKey();
-                    FirebaseDatabase.getInstance().getReference("Users/" + mToUserId + "/Cards").child(cardId).setValue(card);
-                }
-                Toast.makeText(mContext, "Cards added successfully!", Toast.LENGTH_SHORT).show();
+                addCards(cards);
+                FirebaseDatabase.getInstance().getReference("Users/" + mFromUserId + "/Cards").removeEventListener(this);
             }
 
             @Override
@@ -65,5 +57,14 @@ public class NotificationAgreeListener extends BroadcastReceiver {
                 Toast.makeText(mContext,"Fetching user data failed: " + databaseError.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void addCards(Vector<Card> cards)
+    {
+        for(Card card: cards){
+            String cardId = FirebaseDatabase.getInstance().getReference("Users/" + mToUserId + "/Cards").push().getKey();
+            FirebaseDatabase.getInstance().getReference("Users/" + mToUserId + "/Cards").child(cardId).setValue(card);
+        }
+        Toast.makeText(mContext, "Cards added successfully!", Toast.LENGTH_SHORT).show();
     }
 }
